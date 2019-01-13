0bf67e91a73b9f30f6ca3c3f389bd85e2e2cb017
Gabriel de Souza Seibel <gabrielseibel1@hotmail.com>
2019-01-13 09:53:42 -0200

    Add image upload

---
+++ src/Application.kt
@@ -0,0 +1,114 @@
+package com.digity
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.locations.*
+import io.ktor.util.KtorExperimentalAPI
+import java.io.File
+import java.io.IOException
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@KtorExperimentalLocationsAPI
+@Location("/")
+class Upload
+
+
+@KtorExperimentalAPI
+@KtorExperimentalLocationsAPI
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Locations)
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    val config = environment.config.config("digity")
+    val uploadDirPath = config.property("upload.dir").getString()
+    val uploadDir = File(uploadDirPath)
+    if (!uploadDir.mkdirs() && !uploadDir.exists()) {
+        throw IOException("Failed to create directory ${uploadDir.absolutePath}")
+    }
+
+    routing {
+        upload(uploadDir)
+
+        get("/") {
+            call.respondHtml {
+                body {
+                    h1 { +"Moreca" }
+                    p { +"Esse é o site da mozi." }
+                }
+            }
+        }
+
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.red
+                }
+                p {
+                    fontSize = 2.em
+                }
+                rule("p.myclass") {
+                    color = Color.blue
+                }
+            }
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
+fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+    style(type = ContentType.Text.CSS.toString()) {
+        +CSSBuilder().apply(builder).toString()
+    }
+}
+
+fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+    this.style = CSSBuilder().apply(builder).toString().trim()
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
---
+++ src/Upload.kt
@@ -0,0 +1,120 @@
+package com.digity
+
+import com.google.gson.Gson
+import io.ktor.application.*
+import io.ktor.html.*
+import io.ktor.http.content.*
+import io.ktor.locations.*
+import io.ktor.network.util.ioCoroutineDispatcher
+import io.ktor.request.receiveMultipart
+import io.ktor.response.respondText
+import io.ktor.routing.*
+import kotlinx.coroutines.*
+import kotlinx.html.*
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+
+
+@KtorExperimentalLocationsAPI
+fun Route.upload(uploadDir: File) {
+
+    /**
+     * Route to serve HTML to upload file
+     */
+    get<Upload> {
+        call.respondHtml {
+            head {
+                title { +"Upload Digit" }
+            }
+            body {
+                h1 { +"Upload Digit" }
+                p {
+                    +"Please choose an image that contains the drawing of a single digit."
+                }
+
+                form(
+                    action = call.url(Upload()),
+                    encType = FormEncType.multipartFormData,
+                    method = FormMethod.post,
+                    classes = "pure-form-stacked"
+                ) {
+                    acceptCharset = "utf-8"
+
+                    fileInput { name = "file"; accept = "image/jpg, image/png" }
+                    submitInput(classes = "pure-button pure-button-primary") { value = "Upload" }
+                }
+            }
+        }
+    }
+
+    /**
+     * Route to process uploaded file
+     */
+    post<Upload> {
+        val multipart = call.receiveMultipart()
+        var title = String()
+        var imageFile: File? = null
+
+        multipart.forEachPart { part ->
+            when (part) {
+                is PartData.FormItem -> {
+                    if (part.name == "title") title = part.value
+                }
+
+                //copy part content to server-side file
+                is PartData.FileItem -> {
+                    val extension = File(part.originalFileName).extension
+                    val file = File(
+                        uploadDir,
+                        "upload-${System.currentTimeMillis()}-${title.hashCode()}.$extension"
+                    )
+
+                    //open/close input and output streams to copy one to the other
+                    part.streamProvider().use { inputStream ->
+                        file.outputStream().buffered().use { outputStream ->
+
+                            inputStream.copyToSuspend(outputStream)
+                        }
+                    }
+
+                    imageFile = file
+                }
+            }
+
+            part.dispose()
+        }
+
+        call.respondText { Gson().toJson(imageFile) }
+    }
+}
+
+/**
+ * Copies [this] [InputStream] into [out] [OutputStream] in a suspending coroutine
+ *
+ * [bufferSize] and [yieldSize] control how and when the suspending is performed.
+ * [dispatcher] allows to specify where the coroutine will be executed (for example a specific thread pool).
+ */
+suspend fun InputStream.copyToSuspend(
+    out: OutputStream,
+    bufferSize: Int = DEFAULT_BUFFER_SIZE,
+    yieldSize: Int = 4 * 1024 * 1024,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO
+): Long {
+    return withContext(dispatcher) {
+        val buffer = ByteArray(bufferSize)
+        var bytesCopied = 0L
+        var bytesAfterYield = 0L
+        while (true) {
+            val bytes = read(buffer).takeIf { it >= 0 } ?: break
+            out.write(buffer, 0, bytes)
+            if (bytesAfterYield >= yieldSize) {
+                yield()
+                bytesAfterYield %= yieldSize
+            }
+            bytesCopied += bytes
+            bytesAfterYield += bytes
+        }
+        return@withContext bytesCopied
+    }
+}
