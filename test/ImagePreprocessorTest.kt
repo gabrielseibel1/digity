af82ff662425650b4244280a075265ec12fd7982
Gabriel Seibel <gabrielseibel1@gmail.com>
2019-01-19 02:15:09 -0200

    Use updated functions in test

--- test/ImagePreprocessorTest.kt
+++ test/ImagePreprocessorTest.kt
@@ -6,6 +6,6 @@ class ImagePreprocessorTest {
     @Test
     fun testResizeTo28x28() {
         val ipp = ImagePreprocessor()
-        ipp.resizeTo28x28("image/lena1.png")
+        ipp.getFloatArray("../lena1.png")
     }
 }
